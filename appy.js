4add56394afd09d25baaebc43ad9e505e8e7d833
apophis51 <malcolmxvernon@hotmail.com>
2023-12-08 02:48:46 -0500

    update

--- appy.js
+++ appy.js
@@ -8,11 +8,19 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.send(JSON.stringify({ message: 'welcome' }));
 
-
   // Listen for messages from clients
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    ws.send(JSON.stringify({ message: 'we recieved your message' }));
+    ws.send(JSON.stringify({ message: 'we received your message' }));
+
+    // Parse the incoming message
+    const parsedMessage = JSON.parse(message);
+
+    // Check for the 'terminalEvent' type
+    if (parsedMessage.type === 'terminalEvent') {
+      // Respond to the terminal event
+      ws.send(JSON.stringify({ message: 'Response to terminal event', type: 'terminalEvent' }));
+    }
 
     // Broadcast the message to all connected clients
     wss.clients.forEach((client) => {
